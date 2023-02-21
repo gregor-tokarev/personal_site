bc37c622039853e40b3e98d905b55460d3552ff1
gregor <e@tokarev.work>
2023-02-21 17:10:21 +0300

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,2 @@
 import '../../.gt_layout_template/webpCheck';
 import '../blocks'
-// import {Scrollbar} from "smooth-scrollbar/scrollbar";
-//
-// Scrollbar.init(document.querySelector(".main"), {
-//     dumping: 1
-// })
