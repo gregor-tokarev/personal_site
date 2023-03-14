6cf28e832fb15c5bc25cb737cda96e40f63e6a0f
gregor <e@tokarev.work>
2023-03-14 18:00:13 +0300

    fulfill projects section on home page with content and design

--- src/blocks/projects/projects.js
+++ src/blocks/projects/projects.js
@@ -7,7 +7,7 @@ const projects = document.querySelectorAll('.project')
 
 function getTotalScroll() {
     return Array.from(projects).reduce((acc, project) => {
-        acc += project.offsetHeight
+        acc += project.querySelector('.project__wrapper').offsetHeight
         return acc
     }, 0)
 }
@@ -25,5 +25,13 @@ const tl = gsap.timeline({
 projects.forEach((project, idx) => {
     const nextSection = projects[idx + 1]
     if (!nextSection) return
-    tl.from(nextSection, { yPercent: 100, ease: 'linear' })
+    tl.fromTo(nextSection, { maxHeight: '0%' }, { maxHeight: '100%', ease: 'linear' })
+})
+
+// const bottomPadding = document.offsetWidth > 680 ? 80 : 80;
+const bottomPadding = 80
+
+projects.forEach(project => {
+    const body = project.querySelector('.project__body')
+    body.style.top = `${project.offsetHeight - (body.offsetHeight + bottomPadding)}px`
 })
