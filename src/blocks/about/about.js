2c332afe107e6f92a1f8850bf9b24c7bc24e8472
gregor <e@tokarev.work>
2023-03-21 08:29:16 +0300

    add animation for text typing

---
+++ src/blocks/about/about.js
@@ -0,0 +1,30 @@
+import { gsap } from 'gsap';
+import { TextPlugin } from 'gsap/TextPlugin'
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(TextPlugin, ScrollTrigger)
+
+const aboutSection = document.querySelector('.about')
+
+const paragraphs = aboutSection.querySelectorAll('p')
+
+const pageHeight = window.innerHeight;
+
+paragraphs.forEach(p => {
+    const text = p.textContent
+    gsap.set(p, { height: p.offsetHeight, text: '' });
+
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: p,
+            start: `top-=${pageHeight / 3}px center`,
+            end: `bottom-=${pageHeight / 3}px center`,
+            scrub: true,
+            markers: true
+        }
+    })
+
+    for (let i = 0; i < text.length; i++) {
+        tl.to(p, { duration: 10, text: text.slice(0, i + 1), ease: 'none' })
+    }
+})
