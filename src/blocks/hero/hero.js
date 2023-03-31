32ac6547bc692b5ad9836153c3a19494031e6b2f
gregor <e@tokarev.work>
2023-03-31 16:24:21 +0300

    fix too fast fadeout of quotes

--- src/blocks/hero/hero.js
+++ src/blocks/hero/hero.js
@@ -1,20 +1,22 @@
-import { gsap } from 'gsap';
-
 const quoteContent = document.querySelector('[data-hero-quote]')
-
-const tl = gsap.timeline({ repeat: -1, repeatRefresh: true })
 
 if (quoteContent) {
     const quotes = quoteContent.querySelectorAll('p')
+    const quotesCount = quotes.length
 
-    quotes.forEach((q, i) => tl
-        .to(q, { opacity: 1, duration: 0.200 })
-        .to(q, { opacity: 0 }, `+=${i * 5}`))
+    let currentIdx = 0
 
-    quoteContent.addEventListener('mouseenter', () => {
-        tl.pause()
-    })
-    quoteContent.addEventListener('mouseleave', () => {
-        tl.resume()
-    })
+    setInterval(() => {
+        quotes.forEach(q => {
+            q.style.opacity = '0';
+        })
+
+        quotes.item(currentIdx).style.opacity = '1'
+
+        currentIdx++
+
+        if (currentIdx > quotesCount - 1) {
+            currentIdx = 0
+        }
+    }, 5000)
 }
